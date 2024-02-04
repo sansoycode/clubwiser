6a5f728f72154cb1bdc509b17814463c966496b9
sansoycode <36538998+sansoycode@users.noreply.github.com>
2024-02-04 16:59:29 +0000

    Update y.js
    Adding optional params

--- y.js
+++ y.js
@@ -1,10 +1,51 @@
-var iframeHtml="<iframe id=\"the-iframe\" frameborder=\"0\" style=\"position: fixed;top: 0;left: 0;right: 0;bottom: 0;width: 100%;height: 100%;\"></iframe>"
-window.stop()
-document.body.innerHTML=""
-document.body.innerHTML=iframeHtml;
-const iframe = document.getElementById('the-iframe');
 var urlParams = new URLSearchParams(location.search);
 var source;
+var display;
+
+if(urlParams.has('Source')){
+	
+	var landingPages = [
+	  "https://anytimefitnesswoolwich.lpages.co/angel-general-enquiry/", // standard
+	  "https://anytimefitnesswoolwich.lpages.co/anytime-fitness-angel-promotion-price/", // displays price 
+	  "https://anytimefitnesswoolwich.lpages.co/anytime-fitness-angel-promotion-discount/" // displays discount percent
+	];
+	
+	var iframeHtml="<iframe id=\"the-iframe\" frameborder=\"0\" style=\"position: fixed;top: 0;left: 0;right: 0;bottom: 0;width: 100%;height: 100%;\"></iframe>"	
+	window.stop()
+	document.body.innerHTML=""
+	document.body.innerHTML=iframeHtml;
+	const iframe = document.getElementById('the-iframe');
+	
+	var landingPageUrl = landingPages[0];
+	
+	if(urlParams.has('Display')){
+		display = urlParams.get('Display');
+	}
+	if(display){
+		if(display.indexOf("P")>=0){
+			// price is shown
+			landingPageUrl = landingPages[1];	
+		}
+		if(display.indexOf("D")>=0){
+			// discount is shown
+			landingPageUrl = landingPages[2];	
+		}
+		if(display.indexOf("R")>=0){
+			// random display
+			landingPageUrl = myArray[Math.floor(Math.random()*landingPages.length)];
+		}
+	}
+	
+	source = urlParams.get('Source');	
+	if(source){
+		landingPageUrl = landingPageUrl + "?Source=" + source
+	}
+	iframe.src = landingPageUrl;
+}
+
+
+// dont need to track the referrer for now
+/*
 if(document.referrer && 0 !== document.referrer.length){
 	source = new URL(document.referrer).hostname;
 	if(source.indexOf("google")>0){
@@ -20,11 +61,4 @@ if(document.referrer && 0 !== document.referrer.length){
 		source = "Facebook";	
 	}
 }
-if(urlParams.has('Source')){
-	source = urlParams.get('Source');
-}
-var landingPageUrl = "https://anytimefitnesswoolwich.lpages.co/angel-general-enquiry/";
-if(source){
-    landingPageUrl = landingPageUrl + "?Source=" + source
-}
-iframe.src = landingPageUrl;
+*/
