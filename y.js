6755cdbb2f9b318917214322cf21e1c7d8601fd9
sansoycode <36538998+sansoycode@users.noreply.github.com>
2024-03-14 22:39:26 +0000

    Update y.js

--- y.js
+++ y.js
@@ -2,7 +2,7 @@ var urlParams = new URLSearchParams(location.search);
 var source;
 var display;
 
-if(urlParams.has('Source')){
+if(urlParams.has('Source') || urlParams.has('Forward')){
 	
 	var landingPages = [
 	  "https://anytimefitnesswoolwich.lpages.co/angel-general-enquiry/", // standard
