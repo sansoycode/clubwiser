8cfd3ed50058ab133b235ce337029d911a300210
sansoycode <36538998+sansoycode@users.noreply.github.com>
2024-04-29 01:22:23 +0100

    Update y.js

--- y.js
+++ y.js
@@ -1,99 +1,61 @@
 var urlParams = new URLSearchParams(location.search);
 var source;
-var display;
 
-if(urlParams.has('Join')){
-	window.stop();
-	var label = urlParams.get('label');
-	var id = urlParams.get('id');
-	var joinUrl = "https://joinanytimefitnesspresale1.clubwise.com/angel/pos.asp";
-	var seed = id + ",9789";
-	var token = btoa(seed);
-	var customUrl = joinUrl + "?d=" + token;
-	const zapier = "https://hooks.zapier.com/hooks/catch/2629410/3xvcjpq/silent/?id=" + id + "&label=" + label + "&url=" + customUrl;
-	// log the redirect
-	fetch(zapier).then(data => {
-		console.log("Sent to zapier" + data);
-   	 })
-	window.location.href = customUrl;
+var imgElement = document.querySelector('img[title="Anytime-Fitness-Gym-1200×800"]');
+// Check if the element was found
+if (imgElement) {
+	console.log("IMG element found with title 'Anytime-Fitness-Gym-1200×800'");
+	// Get the parent element
+	var parentDiv = imgElement.parentNode;
+
+	// Replace the content of the parent element
+	parentDiv.innerHTML = '<iframe width="560" height="315" src="https://www.youtube.com/embed/BniPPBkoTjw?si=WqHJyUj7kgOZOsww title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>';
+}
+else{
+	console.log("No IMG element found with title 'Anytime-Fitness-Gym-1200×800'");
 }
 
-if(urlParams.has('Fwd')){
-	window.stop();
-	var label = urlParams.get('label');
-	var id = urlParams.get('id');
-	var url = urlParams.get('url');
-	//var tracker = "https://sansoy.co.uk/link.php?id="+id+"&label="+label+"&url="+url;
-	const zapier = "https://hooks.zapier.com/hooks/catch/2629410/3xvcjpq/silent/?id=" + id + "&label=" + label + "&url=" + url;
-	// log the redirect
-	fetch(zapier).then(data => {
-		console.log("Sent to zapier" + data);
-    })
-	window.location.href = url;
+var descriptionElement = document.querySelector('div[data-id="79dde3fb"]');
+if (descriptionElement) {
+	console.log("DIV element found with data-id '79dde3fb'");
+
+	// Replace the content of the element
+	descriptionElement.innerHTML = `
+Our Premium 24 Hour gym is opening in London - Angel, Islington on Monday 13th May 2024! 
+<br/><br/>
+We have brand new high end equipment, including 8 squat racks, 4 studios, functional training space and lots more. Our 50+ classes are included in your membership as is 24/7 access to all 5000 gyms around the world.
+<br/><br/>
+The next 100 joiners get the fantastic price of just <span class="h5">£34.95 per month</span>. <a href="https://joinanytimefitnesspresale1.clubwise.com/angel/pos.asp">Click here to join</a> and take advantage of this offer before it's too late!
+<br/><br/>
+We are so confident you will love our beautiful gym, we even offer a 14 day cooling off period. There is no reason not to lock in your promo rate now and give us a try.
+<br/><br/>
+You can find us located just around the corner of Angel Tube Station on City Road, London, EC1V 1LR
+<br/><br/>
+<iframe src="https://www.google.com/maps/embed?pb=!1m16!1m12!1m3!1d1475.7914288374914!2d-0.1051937412932726!3d51.532010220378226!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!2m1!1sanytime%20fitness%20angel!5e0!3m2!1sen!2suk!4v1714348973258!5m2!1sen!2suk" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+<br/><br/>`;
+	
+}
+else{
+	console.log("No DIV element found with data-id '79dde3fb'");
 }
 
-if(urlParams.has('Source') || urlParams.has('Land')){
+
+// log the visit
 	
-	var landingPages = [
-	  "https://anytimefitnesswoolwich.lpages.co/angel-general-enquiry/", // standard
-	  "https://anytimefitnesswoolwich.lpages.co/anytime-fitness-angel-promotion-price/", // displays price 
-	  "https://anytimefitnesswoolwich.lpages.co/anytime-fitness-angel-promotion-discount/", // displays discount percent
-	  "https://afangelv2.social-fitness.co/coming-soon-ga" // social fitness landing page
-	];
-	
-	var iframeHtml="<iframe id=\"the-iframe\" frameborder=\"0\" style=\"position: fixed;top: 0;left: 0;right: 0;bottom: 0;width: 100%;height: 100%;\"></iframe>"	
-	window.stop()
-	document.body.innerHTML=""
-	document.body.innerHTML=iframeHtml;
-	const iframe = document.getElementById('the-iframe');
-	
-	var landingPageUrl = landingPages[0];
-	
-	if(urlParams.has('Display')){
-		display = urlParams.get('Display');
-	}
-	if(display){
-		if(display.indexOf("P")>=0){
-			// price is shown
-			landingPageUrl = landingPages[1];	
-		}
-		if(display.indexOf("D")>=0){
-			// discount is shown
-			landingPageUrl = landingPages[2];	
-		}
-		if(display.indexOf("R")>=0){
-			// random display
-			landingPageUrl = landingPages[Math.floor(Math.random()*landingPages.length)];
-		}
-		if(display.indexOf("L")>=0){
-			// social fitness landing page
-			landingPageUrl = landingPages[3] + location.search;
-		}
-	}
-	
+var source = "";
+var referrer = "";
+
+if(urlParams.has('Source')){
 	source = urlParams.get('Source');	
-	if(source){
-		landingPageUrl = landingPageUrl + "?Source=" + source
-	}
-	iframe.src = landingPageUrl;
 }
 
-
-// dont need to track the referrer for now
-/*
 if(document.referrer && 0 !== document.referrer.length){
-	source = new URL(document.referrer).hostname;
-	if(source.indexOf("google")>0){
-		source = "Google Search";	
-	}
-	if(source.indexOf("bing")>0){
-		source = "Bing Search";	
-	}
-	if(source.indexOf("yahoo")>0){
-		source = "Yahoo Search";	
-	}
-	if(source.indexOf("facebook")>0){
-		source = "Facebook";	
-	}
+	referrer = new URL(document.referrer).hostname;
 }
-*/
+
+var site = "HomePage"
+var zapier = "https://hooks.zapier.com/hooks/catch/2629410/37sj45d/silent/?site=" + site + "&source=" + source + "&referrer=" + referrer;
+// log the redirect
+fetch(zapier).then(data => {
+	console.log("Sent to zapier" + data);
+});
