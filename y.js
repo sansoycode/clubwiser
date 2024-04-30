e21c6af8dcabace09e91a2a241b25a829ff25ab7
sansoycode <36538998+sansoycode@users.noreply.github.com>
2024-04-30 11:38:24 +0100

    Update y.js

--- y.js
+++ y.js
@@ -39,18 +39,18 @@ window.onload = function() {
 	
 		// Replace the content of the element
 		descriptionElement.innerHTML = `
-	Our Premium 24 Hour gym is opening in London - Angel, Islington on Monday 13th May 2024! 
-	<br/><br/>
+		Our Premium 24 Hour gym is opening in London - Angel, Islington on Monday 13th May 2024! 
+	<br><br>
+    The next 100 joiners get the fantastic price of just <span class="h5">£34.95 per month</span>. <h5><a href="https://joinanytimefitnesspresale1.clubwise.com/angel/pos.asp">Click here to join</a></h5> and take advantage of this offer before it's too late!
+    <br><br>
+    Not ready to sign up right now? No problem:  <h5><a href="https://calendly.com/atf-angel/club-tour">Click here to book a personal tour of the beautiful club</a>!</h5> We can't wait to show you around.
+	<br><br>
 	We have brand new high end equipment, including 8 squat racks, 4 studios, functional training space and lots more. Our 50+ classes are included in your membership as is 24/7 access to all 5000 gyms around the world.
-	<br/><br/>
-	The next 100 joiners get the fantastic price of just <span class="h5">£34.95 per month</span>. <a href="https://joinanytimefitnesspresale1.clubwise.com/angel/pos.asp">Click here to join</a> and take advantage of this offer before it's too late!
-	<br/><br/>
+	<br><br>
 	We are so confident you will love our beautiful gym, we even offer a 14 day cooling off period. There is no reason not to lock in your promo rate now and give us a try.
-	<br/><br/>
+	<br><br>
 	You can find us located just around the corner of Angel Tube Station on City Road, London, EC1V 1LR
-	<br/><br/>
-	<iframe src="https://www.google.com/maps/embed?pb=!1m16!1m12!1m3!1d1475.7914288374914!2d-0.1051937412932726!3d51.532010220378226!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!2m1!1sanytime%20fitness%20angel!5e0!3m2!1sen!2suk!4v1714348973258!5m2!1sen!2suk" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
-	<br/><br/>`;
+	<br><br>`;
 		
 	}
 	else{
