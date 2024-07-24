49ac1cc8dd78e5826313cd721058e4fb1fcd9cc4
sansoycode <36538998+sansoycode@users.noreply.github.com>
2024-07-24 10:04:18 +0100

    Update z.js

--- z.js
+++ z.js
@@ -2,8 +2,46 @@ var urlParams = new URLSearchParams(location.search);
 var source = "Web";
 
 if(urlParams.has('Source')){
-	source = urlParams.get('Source');	
+	source = urlParams.get('Source');
+	
+	var sectionElement = document.querySelector('section[data-id="7e41a3b3"]');
+	var formUrl = "https://www.atfpromo.co.uk/websiteform/?Source=" + source;
+	if (sectionElement) {
+		console.log("Section element found with data-id '7e41a3b3'");
+		// remove the old freetrial tag
+		var freetrialElement = document.getElementById("freetrial");
+		freetrialElement.parentNode.removeChild(freetrialElement);
+		// freetrialElement.id = "oldFreetrial";
+		sectionElement.innerHTML = '<a href="" id="freetrial"></a><iframe id="myIframe" src="'+formUrl+'" scrolling="no" onload="resizeIframe()"></iframe>';
+	}
+	else{
+		console.log("No Section element found with data-id '7e41a3b3'");
+	}
+	
+	var mobileSectionElement = document.querySelector('section[data-id="0bed894"]');
+	if (mobileSectionElement) {
+		console.log("mobile Section element found with data-id '0bed894'");
+		mobileSectionElement.parentNode.removeChild(mobileSectionElement);
+	}
+	else{
+		console.log("No mobile Section element found with data-id '0bed894'");
+	}
+	
+	function resizeIframe() {
+		var iframe = document.getElementById('myIframe');
+		iframe.style.height = iframe.contentWindow.document.body.scrollHeight + 'px';
+	}
+	
+	// Optional: Resize the iframe when the content inside it changes
+	window.addEventListener('message', function(event) {
+		if (event.data && event.data.iframeHeight) {
+			var iframe = document.getElementById('myIframe');
+			iframe.style.height = event.data.iframeHeight + 'px';
+		}
+	}, false);
+	
 }
+/**	
 else if(document.referrer && 0 !== document.referrer.length){
 	source = new URL(document.referrer).hostname.toLowerCase();
 	if(source.indexOf("google")>0){
@@ -21,42 +59,9 @@ else if(document.referrer && 0 !== document.referrer.length){
 	if(source.indexOf("instagram")>0){
 		source = "Instagram";	
 	}
-  if(source.indexOf("anytimefitness.co")>0){
+  	if(source.indexOf("anytimefitness.co")>0){
 		source = "AnytimeFitnessHomepage";	
 	}
 }
+**/
 
-var sectionElement = document.querySelector('section[data-id="7e41a3b3"]');
-var formUrl = "https://www.atfpromo.co.uk/websiteform/?Source=" + source;
-if (sectionElement) {
-	console.log("Section element found with data-id '7e41a3b3'");
-	// remove the old freetrial tag
-	var freetrialElement = document.getElementById("freetrial");
-	freetrialElement.id = "oldFreetrial";
-	sectionElement.innerHTML = '<a href="" id="freetrial"></a><iframe id="myIframe" src="'+formUrl+'" scrolling="no" onload="resizeIframe()"></iframe>';
-}
-else{
-	console.log("No Section element found with data-id '7e41a3b3'");
-}
-
-var mobileSectionElement = document.querySelector('section[data-id="0bed894"]');
-if (mobileSectionElement) {
-	console.log("mobile Section element found with data-id '0bed894'");
-	mobileSectionElement.parentNode.removeChild(mobileSectionElement);
-}
-else{
-	console.log("No mobile Section element found with data-id '0bed894'");
-}
-
-function resizeIframe() {
-	var iframe = document.getElementById('myIframe');
-	iframe.style.height = iframe.contentWindow.document.body.scrollHeight + 'px';
-}
-
-// Optional: Resize the iframe when the content inside it changes
-window.addEventListener('message', function(event) {
-	if (event.data && event.data.iframeHeight) {
-		var iframe = document.getElementById('myIframe');
-		iframe.style.height = event.data.iframeHeight + 'px';
-	}
-}, false);
